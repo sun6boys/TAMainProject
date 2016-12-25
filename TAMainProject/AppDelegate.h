7f0cb2abe2c60b0706be19ff15cf9c73f9781327
15370989688 <15370989688@163.com>
2016-12-25 15:01:06 +0800

    Initial Commit

---
+++ TAMainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TAMainProject
+//
+//  Created by Charon on 16/12/25.
+//  Copyright © 2016年 Charon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
